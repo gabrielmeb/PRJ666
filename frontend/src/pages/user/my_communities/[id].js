3f5c4402b19b2051773096d4f63b0c49b900710a
manav zadafiya <manavzadafiya@gmail.com>
2025-04-08 22:08:34 -0400

    wrapped in Layout

--- frontend/src/pages/user/my_communities/[id].js
+++ frontend/src/pages/user/my_communities/[id].js
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 import { useEffect, useRef, useState } from "react";
 import io from "socket.io-client";
 import { apiFetch, API_URL } from "@/utils/api";
+import Layout from "@/components/Layout";
 
 export default function CommunityDetailPage() {
   const router = useRouter();
@@ -100,6 +101,7 @@ export default function CommunityDetailPage() {
   };
 
   return (
+    <Layout>
     <div className="max-w-3xl mx-auto p-4">
       {/* Community name on top */}
       {community ? (
@@ -141,5 +143,6 @@ export default function CommunityDetailPage() {
         </button>
       </form>
     </div>
+    </Layout>
   );
 }
